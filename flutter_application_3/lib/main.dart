f22f565b94d531b8e1cc5664db7c7ad0fc4597e3
R0g3r <rogegolbe@gmail.com>
2025-03-17 14:57:01 -0500

    Mejorando NavBottomBar

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -1,35 +1,57 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_example/core/main_colors.dart';
 import 'package:flutter_application_example/screens/user_screens/main_user_screen.dart';
+import 'package:flutter_application_example/screens/user_screens/map_user_screen.dart';
+import 'package:flutter_application_example/screens/user_screens/user_profile_screen.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int index = 1;
+
+  final screens = [
+    MapUserScreen(),
+    UserHomeScreen(),
+    UserProfileScreen()
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(Icons.map, size: 30),
+      Icon(Icons.home, size: 30),
+      Icon(Icons.account_circle, size: 30)
+    ];
+
+
     return MaterialApp(
       home: Scaffold(
+        extendBody: true,
         body: Center(
-          child: UserHomeScreen(),
+          child: screens[index],
         ),
-        bottomNavigationBar:
-            BottomNavigationBar(items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.tiktok),
-            label: "TikTok Obvi",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          )
-        ]),
+        bottomNavigationBar: CurvedNavigationBar(
+          color: AppColors.backgroundLogin,
+          backgroundColor: Colors.transparent,
+          items: items,
+          height: 60,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          index: index,
+          onTap: (index) => setState(() {
+            this.index = index;
+          }),
+        ),
       ),
     );
   }
---
+++ flutter_application_3/lib/screens/user_screens/map_user_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MapUserScreen extends StatefulWidget {
+  const MapUserScreen({super.key});
+
+  @override
+  State<MapUserScreen> createState() => _MapUserScreenState();
+}
+
+class _MapUserScreenState extends State<MapUserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text("MAPA AQUI"),
+    );
+  }
+}
