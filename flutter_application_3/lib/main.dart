2d70ccc2c4efd722701ce0dd48ac3295f6aabb24
PaoloLuw <114841110+PaoloLuw@users.noreply.github.com>
2025-03-18 12:48:06 -0500

    -------

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart'; // Importa el widget raíz
-import '/presentation/screens/owner_screens/main_owner_screen.dart';
 
 void main() {
-  runApp(OwnerHomeScreen()); 
+  runApp(const App()); 
 }
